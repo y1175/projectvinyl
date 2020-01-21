998406e56028e6ad9967dcf027b4ef5cf04db24b
JINNY <JINNY@DESKTOP-3GE25F1>
2020-01-21 16:59:57 +0900

    eferq

--- src/jy/com/homedream/service/JY_BuyService.java
+++ src/jy/com/homedream/service/JY_BuyService.java
@@ -41,6 +41,27 @@ public class JY_BuyService {
 		return itemdto;
 	}
 	
+	public JY_ItemDTO getItem(int item_no) {
+		
+		DBConnection db = DBConnection.getInstance();
+		Connection conn = null;
+		JY_ItemDTO itemdto = new JY_ItemDTO();
+
+		try {
+			conn = db.getConnection();
+			conn.setAutoCommit(false);
+			JY_BuyDAO buydao = JY_BuyDAO.getDAO();
+			itemdto = buydao.selectItem(conn, item_no);
+			conn.commit();
+		} catch(SQLException | NamingException e) {
+			System.out.println(e);
+			try {conn.rollback();} catch(SQLException e2) {}
+		}finally {
+			if(conn!=null) try {conn.close();} catch(SQLException e) {}
+		}
+		return itemdto;
+	}
+	
 	public JY_JN_MemberDTO getBuyMember(int mem_no) {
 		
 		DBConnection db = DBConnection.getInstance();
